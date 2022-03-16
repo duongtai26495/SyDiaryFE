edde647fcdb275affaebb2b4e0d8d777c26ccdbc
duongtai26495 <taiduong.264@gmail.com>
2022-03-17 00:22:18 +0700

    Test api

--- App.js
+++ App.js
@@ -1,112 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { View, Text, SafeAreaView, Image, TouchableOpacity, TextInput } from 'react-native'
+import React,{useState, useRef} from 'react'
+import axios from 'axios'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [username, setUsername] = useState()
+  const [email, setEmail] = useState()
+  const [fullName, setFullName] = useState()
+  const [password, setPassword] = useState()
+  const [status, setStatus] = useState()
+  
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const Register = async () =>{
+      const User ={
+        fullName : fullName,
+        email : email,
+        password : password,
+        username : username
+      }
+        const api_url = "http://192.168.1.15:8080/user/register"
+        await axios.post(api_url, User)
+          .then((response)=>{
+            setStatus(response.data.msg)
+          })
+          .catch((error)=>{
+            if (error.response) {
+              setStatus(error.response.data.msg)
+            }
+          })
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View style={{padding:10,backgroundColor:'rgba(255,255,255,1)'}}>
+          <TextInput 
+          placeholder='Full name' 
+          underlineColorAndroid={'rgba(20,20,20,1)'}
+          style={{color:'rgba(0,0,0,1)'}} 
+          placeholderTextColor={'rgba(0,0,0,1)'}
+          onChangeText={(value)=>{setFullName(value)}}/>
+          <TextInput 
+          placeholder='Username' 
+          style={{color:'rgba(0,0,0,1)'}} 
+          placeholderTextColor={'rgba(0,0,0,1)'}
+          underlineColorAndroid={'rgba(20,20,20,1)'}
+          onChangeText={(value)=>{setUsername(value)}}/>
+          <TextInput 
+          placeholder='Email'
+          style={{color:'rgba(0,0,0,1)'}} 
+          placeholderTextColor={'rgba(0,0,0,1)'}
+          underlineColorAndroid={'rgba(20,20,20,1)'}
+          onChangeText={(value)=>{setEmail(value)}}/>
+          <TextInput 
+          placeholder='Password' 
+          secureTextEntry={true} 
+          placeholderTextColor={'rgba(0,0,0,1)'}
+          style={{color:'rgba(0,0,0,1)'}} 
+          underlineColorAndroid={'rgba(20,20,20,1)'} 
+          onChangeText={(value)=>{setPassword(value)}}/>
+          <TouchableOpacity 
+          style={{backgroundColor:'rgba(244,233,11,1)', padding:10, borderRadius:5,elevation:5, shadowColor:'rgba(22,32,0,1)'}}
+          onPress={()=>{Register()}}>
+            <Text style={{color:'rgba(0,0,0,1)'}} >Register</Text>
+          </TouchableOpacity>
+          <Text style={{margin:10,color:'rgba(100,0,10,1)', fontSize:20, fontWeight:'bold', alignSelf:'center'}}>{status}</Text>
+      </View> 
     </SafeAreaView>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
